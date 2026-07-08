288a7c58aaf99a130a35c28ab9fe35a947ade3ab
Satyam-k-jha <skumar65321@gmail.com>
2026-07-08 23:35:14 +0530

    Registered few interfaces

--- MessManagementSystem/Program.cs
+++ MessManagementSystem/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddSwaggerGen();
 //Registering the services for dbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//Registering Automapping
 builder.Services.AddAutoMapper(cfg => { }, typeof(AutoMapperProfiles));
 
 //Registering Interfaces from repository
@@ -26,6 +27,9 @@ builder.Services.AddScoped<IFoodRepository, FoodRepository>();
 builder.Services.AddScoped<IMenuRepository, MenuRepository>();
 builder.Services.AddScoped<IAttendanceRepository, AttendanceRepository>();
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IAttendanceService, AttendanceService>();
+builder.Services.AddScoped<IMenuService, MenuService>();
+builder.Services.AddScoped<IFoodService, FoodService>();
 
 var app = builder.Build();
 
